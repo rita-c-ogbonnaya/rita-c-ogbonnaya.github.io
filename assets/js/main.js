49c84e242923bad58de7e1f81c8e56c113eec119
Rita C Ogbonnaya <dcodegod@gmail.com>
2024-12-15 20:33:18 +0100

    my portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,12 +42,12 @@
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+  // const preloader = document.querySelector('#preloader');
+  // if (preloader) {
+  //   window.addEventListener('load', () => {
+  //     preloader.remove();
+  //   });
+  // }
 
   /**
    * Scroll top button
